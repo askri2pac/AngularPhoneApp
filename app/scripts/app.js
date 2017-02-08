6ec848c54ce3be88b13e4edcb6f84bd73a51a419
Nassim <nassimaskri@gmail.com>
2017-02-08 21:31:12 +0100

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name firstappApp
+ * @description
+ * # firstappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('firstappApp', []);
